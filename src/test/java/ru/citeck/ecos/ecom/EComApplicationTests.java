696c2b300e8b621d8b372c58ea5ff814df07a2d8
antivanov <anton.ivanov@citeck.ru>
2022-11-10 21:03:44 +0300

    Disable test

--- src/test/java/ru/citeck/ecos/ecom/EComApplicationTests.java
+++ src/test/java/ru/citeck/ecos/ecom/EComApplicationTests.java
@@ -1,8 +1,13 @@
 package ru.citeck.ecos.ecom;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.citeck.ecos.webapp.lib.spring.test.extension.EcosSpringExtension;
 
+@Disabled
+@ExtendWith(EcosSpringExtension.class)
 @SpringBootTest
 class EComApplicationTests {
 
