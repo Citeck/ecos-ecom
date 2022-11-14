6b619d1434eedae99fcf5970646031355a08ccda
Pavel Simonov <pavel.simonov@citeck.ru>
2022-11-14 13:45:27 +0700

    Update to new parent version

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,10 +1,1 @@
-node {
-  jdk = tool name: 'JDK11'
-  env.JAVA_HOME = "${jdk}"
-
-  echo "jdk installation path is: ${jdk}"
-
-  sh "${jdk}/bin/java -version"
-
-  ecosBuild()
-}
+ecosBuild()
--- src/test/java/ru/citeck/ecos/ecom/EComApplicationTests.java
+++ src/test/java/ru/citeck/ecos/ecom/EComApplicationTests.java
@@ -1,12 +1,10 @@
 package ru.citeck.ecos.ecom;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.citeck.ecos.webapp.lib.spring.test.extension.EcosSpringExtension;
 
-@Disabled
 @ExtendWith(EcosSpringExtension.class)
 @SpringBootTest
 class EComApplicationTests {
