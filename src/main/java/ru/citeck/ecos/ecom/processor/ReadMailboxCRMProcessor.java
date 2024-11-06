2930326382675d6bf2cd1be8c0c2c54b0783abde
Daniil Shcherbina <daniil.shcherbina@citeck.ru>
2024-11-06 13:34:46 +0300

    ECOSENT-3185 - CRM. Add new request category

--- src/main/java/ru/citeck/ecos/ecom/processor/ReadMailboxCRMProcessor.java
+++ src/main/java/ru/citeck/ecos/ecom/processor/ReadMailboxCRMProcessor.java
@@ -25,13 +25,15 @@ public class ReadMailboxCRMProcessor implements Processor {
     @Value("${mail.deal.subject.demo-access}")
     private String dealSubjectDemoAccess;
     @Value("${mail.deal.subject.community}")
-    private String dealSubjectCommunity;
+    private String[] dealSubjectCommunity;
     @Value("${mail.deal.subject.price}")
     private String dealSubjectPrice;
     @Value("${mail.deal.subject.cloud}")
     private String dealSubjectCloud;
     @Value("${mail.deal.subject.community-subscription}")
     private String communitySubscription;
+    @Value("${mail.deal.subject.partnership-request}")
+    private String partnershipRequest;
 
     public static final String CONSULT_KIND = "consult";
     public static final String DEMONSTRATION_KIND = "demonstration";
@@ -42,6 +44,7 @@ public class ReadMailboxCRMProcessor implements Processor {
     public static final String COMMUNITY_SUBSCRIPTION_KIND = "community-subscription";
     public static final String OTHER_KIND = "other";
     public static final String EMAIL_KIND = "email";
+    public static final String PARTNERSHIP_REQUEST_KIND = "partnership-request";
 
     private final Pattern dealNumber;
 
@@ -76,35 +79,43 @@ public class ReadMailboxCRMProcessor implements Processor {
             exchange.setProperty("subject", "deal");
         }
 
-        for (String dealSubject : dealSubjectsConsult) {
-            if (mail.getSubject().contains(dealSubject)) {
-                mail.setKind(CONSULT_KIND);
-            }
-        }
-        if (mail.getKind() == null) {
-            for (String dealSubject : dealSubjectsDemonstration) {
+        if (mail.getDealNumber() == null) {
+            for (String dealSubject : dealSubjectsConsult) {
                 if (mail.getSubject().contains(dealSubject)) {
-                    mail.setKind(DEMONSTRATION_KIND);
+                    mail.setKind(CONSULT_KIND);
+                }
+            }
+            if (mail.getKind() == null) {
+                for (String dealSubject : dealSubjectsDemonstration) {
+                    if (mail.getSubject().contains(dealSubject)) {
+                        mail.setKind(DEMONSTRATION_KIND);
+                    }
+                }
+            }
+            if (mail.getKind() == null) {
+                for (String dealSubject : dealSubjectCommunity) {
+                    if (mail.getSubject().contains(dealSubject)) {
+                        mail.setKind(COMMUNITY_KIND);
+                    }
+                }
+            }
+            if (mail.getKind() == null) {
+                if (mail.getSubject().contains(dealSubjectPrice))
+                    mail.setKind(PRICE_KIND);
+                else if (mail.getSubject().contains(dealSubjectDemoAccess))
+                    mail.setKind(DEMO_ACCESS_KIND);
+                else if (mail.getSubject().contains(dealSubjectCloud))
+                    mail.setKind(CLOUD_KIND);
+                else if (mail.getSubject().contains(communitySubscription))
+                    mail.setKind(COMMUNITY_SUBSCRIPTION_KIND);
+                else if (mail.getSubject().contains(partnershipRequest))
+                    mail.setKind(PARTNERSHIP_REQUEST_KIND);
+                else {
+                    mail.setKind(OTHER_KIND);
+                    exchange.setProperty("subject", "other");
                 }
             }
         }
-        if (mail.getKind() == null && mail.getDealNumber() == null) {
-            if (mail.getSubject().contains(dealSubjectCommunity))
-                mail.setKind(COMMUNITY_KIND);
-            else if (mail.getSubject().contains(dealSubjectPrice))
-                mail.setKind(PRICE_KIND);
-            else if (mail.getSubject().contains(dealSubjectDemoAccess))
-                mail.setKind(DEMO_ACCESS_KIND);
-            else if (mail.getSubject().contains(dealSubjectCloud))
-                mail.setKind(CLOUD_KIND);
-            else if (mail.getSubject().contains(communitySubscription))
-                mail.setKind(COMMUNITY_SUBSCRIPTION_KIND);
-            else {
-                mail.setKind(OTHER_KIND);
-                exchange.setProperty("subject", "other");
-            }
-        }
-
         exchange.getIn().setBody(mail);
     }
 
