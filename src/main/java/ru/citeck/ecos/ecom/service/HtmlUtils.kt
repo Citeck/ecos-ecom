30ee5f34d6aad55a77aacc024aa8fc44f7723adf
Pavel Simonov <pavel.simonov@citeck.ru>
2025-05-12 16:53:58 +0700

    ECOSCOM-5786 - SD letter content should not contain images in Base64 format. Release 1.11.1

--- src/main/java/ru/citeck/ecos/ecom/service/HtmlUtils.kt
+++ src/main/java/ru/citeck/ecos/ecom/service/HtmlUtils.kt
@@ -8,11 +8,31 @@ import org.jsoup.select.NodeVisitor
 
 object HtmlUtils {
 
-    private val validTags = setOf("strong", "em", "ul", "li", "ol", "p", "i", "b", "u", "a", "br", "span", "img")
-    private val validAttributes = mapOf(
+    private val VALID_TAGS = setOf("strong", "em", "ul", "li", "ol", "p", "i", "b", "u", "a", "br", "span", "img")
+
+    private val VALID_ATTS_FOR_ALL = setOf(
+        "style"
+    )
+    private val VALID_ATTS_BY_TAG = listOf(
         "a" to setOf("href"),
         "img" to setOf("src", "alt")
-    )
+    ).associate {
+        it.first to setOf(*it.second.toTypedArray(), *VALID_ATTS_FOR_ALL.toTypedArray())
+    }
+
+    private val ALLOWED_INLINE_STYLES = setOf(
+        "font-size",
+        "font-style",
+        "color",
+        "white-space",
+        "grid-template-columns",
+        "width",
+        "height",
+        "border",
+        "vertical-align",
+        "text-align",
+        "padding-inline-start",
+    ).map { "$it:" }
 
     /**
      * Convert the HTML content of the email to a format suitable
@@ -35,21 +55,36 @@ object HtmlUtils {
                         } else {
                             node.replaceWith(TextNode(node.wholeText()))
                         }
-                    } else if (!validTags.contains(tag)) {
+                    } else if (!VALID_TAGS.contains(tag)) {
                         var newNode: Node = TextNode(node.wholeText())
                         if (depth == 1) {
                             newNode = Element("p").appendChild(newNode)
                         }
                         node.replaceWith(newNode)
                     } else {
-                        val validAtts = validAttributes[tag] ?: emptySet()
+                        val validAtts = VALID_ATTS_BY_TAG[tag] ?: emptySet()
                         val iter = node.attributes().iterator()
                         while (iter.hasNext()) {
                             val att = iter.next()
                             if (!validAtts.contains(att.key)) {
                                 iter.remove()
+                            } else if (att.key == "style") {
+                                val styleValue = att.value
+                                if (styleValue.contains("url")) {
+                                    iter.remove()
+                                } else {
+                                    val safeStyle = styleValue.split(";")
+                                        .map { it.trim() }
+                                        .filter { style ->
+                                            ALLOWED_INLINE_STYLES.any { style.startsWith(it) }
+                                        }.joinToString(";")
+                                    if (safeStyle != styleValue) {
+                                        att.setValue(safeStyle)
+                                    }
+                                }
                             }
                         }
+
                     }
                 } else if (node is TextNode && depth == 1) {
                     node.replaceWith(Element("p").appendChild(TextNode(node.wholeText)))
